0a2745497ce4bbb5f9015403c6045855349d6c49
Lando <lando.schumpich@gmail.com>
2019-09-27 19:06:03 +0200

    Simple GH PlugIn with only one component: `Operate`

---
+++ ConwayGH/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayGH
+{
+    public static class Settings
+    {
+        public static string MainCategory = "Conway";
+        public static string SubCategoryManipulate = "Manipulate";
+    }
+}
